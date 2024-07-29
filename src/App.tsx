2965c26499df08ef7bbdb8183308ec6292663036
razzput007 <dhoom.rk74@gmail.com>
2024-07-29 11:12:27 +0530

    Login UI

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,8 @@ const TransactionManagement = lazy(
   () => import("./pages/admin/management/transactionmanagement")
 );
 const Shipping=lazy(()=>import ("./pages/Shipping"))
+
+const Login=lazy(()=>import("./pages/Login"));
 import Header from "./components/Header";
 
 const App = () => {
@@ -35,6 +37,8 @@ const App = () => {
        <Route path="/" element={<Home/>}/>
        <Route path="/search" element={<Search/>}/>
        <Route path="/cart" element={<Cart/>}/>
+
+       <Route path="/login" element={<Login/>}/>
 
        {/*Logged in user routes */}
        <Route>
---
+++ src/pages/Login.tsx
@@ -0,0 +1,40 @@
+import { useState} from 'react'
+import { FcGoogle } from 'react-icons/fc';
+
+const Login = () => {
+
+ const[date,Setdate]=useState("");
+ const [gender,Setgender]=useState("");
+
+ const loginHandler=()=>{
+    console.log(date,gender);
+ }
+
+  return (
+    <div className='login'>
+        <main>
+            <h1 className='heading'>Login</h1>
+            <div>
+                <label>Gender</label>
+                <select value={gender} onChange={(e)=>Setgender(e.target.value)}>
+                     <option value="">Select Gender</option>
+                     <option value="male">Male</option>
+                     <option value="female">Female</option>
+                </select>
+            </div>
+            <div>
+                <label>Date of Birth</label>
+                <input type="date" value={date} onChange={(e)=>Setdate(e.target.value)}/>
+            </div>
+            <div>
+                <p>Already Signed In Once</p>
+                <button onClick={loginHandler}>
+                    <FcGoogle/> <span>Sign in with Google</span>
+                </button>
+            </div>
+        </main>
+    </div>
+  )
+}
+
+export default Login
